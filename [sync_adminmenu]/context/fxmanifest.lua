c961dc9c200bb203b7021ecf4597b0d2fa95c7b7
sync <50116007+synceye@users.noreply.github.com>
2022-09-17 18:27:28 +0200

    Add files via upload

---
+++ [sync_adminmenu]/context/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+client_scripts {
+    'client/client.lua'
+}
+
+ui_page 'ui/index.html'
+
+files {
+    'ui/index.html',
+    'ui/style.css',
+    'ui/script.js'
+}
+
+escrow_ignore {
+    'client/*',
+    'ui/*'
+}
